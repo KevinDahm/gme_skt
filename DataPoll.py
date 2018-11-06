346e3ed9fe7de367d253203830b3ef1995e1d2f5
David Dalo <ddalo2110@gmail.com>
2018-11-05 20:21:44 -0600

    Updated Data Poll Function with Url list

--- DataPoll.py
+++ DataPoll.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as BS
 import urllib3
 import requests
+from urllist import *
 
 def worth(website, line_num, lim):
 
@@ -14,7 +15,28 @@ def worth(website, line_num, lim):
     x = int(a[0])/int(a[1])
     return(x)
 
+#iteration for what price we sell at
+print("Alt per Chaos =", worth(alt_per_chaos,7,8))
+print("Fus per Chaos =", worth(fus_per_chaos,7,8))
+print("Alc per Chaos =", worth(alc_per_chaos,7,8))
+print("Gc per Chaos =", worth(gc_per_chaos,7,8))
+print("Ex per Chaos =", worth(ex_per_chaos,7,8))
+print("Chr per Chaos =", worth(chr_per_chaos,7,8))
+print("Jew per Chaos =", worth(jew_per_chaos,7,8))
+print("Chi per Chaos =", worth(chi_per_chaos,7,8))
+print("Asx per Chaos =", worth(asx_per_chaos,7,8))
+print("Jsx per Chaos =", worth(jsx_per_chaos,7,8))
+print("Msx per Chaos =", worth(msx_per_chaos,7,8))
 
-#iteration for chaos/alterations value of alts per chaos.
-chaos_2_alts = worth('http://currency.poe.trade/search?league=Delve&online=x&stock=&want=1&have=4',7,8)
-print(chaos_2_alts)
+#iteration for what price we buy at
+print("Chaos per Alt =", worth(chaos_per_alt,7,8))
+print("Chaos per Fus =", worth(chaos_per_fus,7,8))
+print("Chaos per Alc =", worth(chaos_per_alc,7,8))
+print("Chaos per Gc =", worth(chaos_per_gc,7,8))
+print("Chaos per Ex =", worth(chaos_per_ex,7,8))
+print("Chaos per Chr =", worth(chaos_per_chr,7,8))
+print("Chaos per Jew =", worth(chaos_per_jew,7,8))
+print("Chaos per Chi =", worth(chaos_per_chi,7,8))
+print("Chaos per Asx =", worth(chaos_per_asx,7,8))
+print("Chaos per Jsx =", worth(chaos_per_jsx,7,8))
+print("Chaos per Msx =", worth(chaos_per_msx,7,8))
